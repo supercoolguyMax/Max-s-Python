bebe2d86c7c06e9e22028b2702ce67e5421f044b
supercoolguyMax <maxcheer@riccarton.school.nz>
2025-03-28 10:14:27 +1300

    assessment try #2

--- Assessment/treasure.py
+++ Assessment/treasure.py
@@ -1,15 +1,36 @@
 """This code will ask for coordinates that will be used to locate a treasure. The coordinates will be used to determine if the treasure is located at the coordinates entered."""
+#treasure list
+treasue_coords = []
+
+#running x input
+
 while True:
-    x_input = input("Enter X coordinate: ")
-    if x_input == "complete":
+    x_input = input("Enter X coordinates: ")
+
+#stopping the code when user inputs complete
+    
+    if x_input.lower == 'complete':
         break
-    y_input = input("Enter Y coordinate: ")
-    if not x_input.isdigit() or not y_input.isdigit():
+#ouputing invaild numbers 
+    try:
+        x = int(x_input)
+    except ValueError:
         print("Error. Invalid.")
         continue
-    x = int(x_input)
-    y = int(y_input)
-    if x + y == 10:
-        print("Treasure located at ({}, {})".format(x, y))
 
-print{'')}
+#running y input
+    try:
+        y_input = input("Enter Y coordinates: ")
+        y = int(y_input)
+    except ValueError
+        print("Error. Invalid.")
+        continue
+
+#adding the x input and the y input for final results 
+    if y + x == 10:   
+        treasue_coords.append((x, y))
+
+    if treasue_coords:
+        print("Treasure located at")
+        for coord in treasure_coords 
+            print(f"({coord{0}}), ({coord{1}})")
