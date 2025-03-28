387b87f1ba292633ca73a124b0c24287992adef0
supercoolguyMax <maxcheer@riccarton.school.nz>
2025-03-28 13:33:22 +1300

    Final Code Assessment

--- Assessment/treasure.py
+++ Assessment/treasure.py
@@ -1,36 +1,34 @@
-"""This code will ask for coordinates that will be used to locate a treasure. The coordinates will be used to determine if the treasure is located at the coordinates entered."""
-#treasure list
-treasue_coords = []
+# treasure variable
+treasure_coords = []
 
-#running x input
-
+# running x input
 while True:
-    x_input = input("Enter X coordinates: ")
-
-#stopping the code when user inputs complete
-    
-    if x_input.lower == 'complete':
+    x_input = input("Enter X coordinate: ")
+    # breaking x input
+    if x_input.lower() == "complete":
         break
-#ouputing invaild numbers 
+    # invalid input handling for X coordinate
     try:
         x = int(x_input)
     except ValueError:
-        print("Error. Invalid.")
+        print("Error. Invalid input. Please enter an integer.")
         continue
-
-#running y input
+    
+    # running y input
+    y_input = input("Enter Y coordinate: ")
+    # invalid input handling for Y coordinate
     try:
-        y_input = input("Enter Y coordinates: ")
         y = int(y_input)
-    except ValueError
-        print("Error. Invalid.")
+    except ValueError:
+        print("Error. Invalid input. Please enter an integer.")
         continue
+    
+    # adding valid x and y inputs if their sum is 10
+    if x + y == 10:
+        treasure_coords.append([x, y])
 
-#adding the x input and the y input for final results 
-    if y + x == 10:   
-        treasue_coords.append((x, y))
-
-    if treasue_coords:
-        print("Treasure located at")
-        for coord in treasure_coords 
-            print(f"({coord{0}}), ({coord{1}})")
+# printing coordinates
+if treasure_coords:
+    print("Treasure located at")
+    for coord in treasure_coords:
+        print(f"({coord[0]}, {coord[1]})")
